1e7f5bff4a8130cc88194fa44427f0f58ff5c589
topseySuave <gabrielsuave17@gmail.com>
2019-06-03 00:29:26 +0100

    Fix tslint issues

--- components/ui/Attributes/Attributes.tsx
+++ components/ui/Attributes/Attributes.tsx
@@ -1,5 +1,5 @@
+import { Button, InputNumber, Radio, Typography } from 'antd';
 import React from 'react';
-import { Typography, Radio, InputNumber, Button } from 'antd';
 import './Attributes.scss';
 
 const { Title } = Typography;
@@ -16,39 +16,77 @@ const Attributes: React.SFC<Props> = ({ product }) => {
   return (
     <>
       <div className="oct-attributes">
-        <Title level={4} style={{ color: '#CCC' }}>Color</Title>
+        <Title level={4} style={{ color: '#CCC' }}>
+          Color
+        </Title>
         <Radio.Group defaultValue="a" buttonStyle="outline">
-          <Radio.Button className="oct-radio-button" value="1" style={{ backgroundColor: '#F62F5E' }} />
-          <Radio.Button className="oct-radio-button" value="2" style={{ backgroundColor: '#6EB2FB' }} />
-          <Radio.Button className="oct-radio-button" value="3" style={{ backgroundColor: '#00D3CA' }} />
-          <Radio.Button className="oct-radio-button" value="4" style={{ backgroundColor: '#FE5C07' }} />
-          <Radio.Button className="oct-radio-button" value="5" style={{ backgroundColor: '#F8E71C' }} />
-          <Radio.Button className="oct-radio-button" value="6" style={{ backgroundColor: '#7ED321' }} />
-          <Radio.Button className="oct-radio-button" value="7" style={{ backgroundColor: '#9013FE' }} />
+          <Radio.Button
+            className="oct-radio-button"
+            value="1"
+            style={{ backgroundColor: '#F62F5E' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="2"
+            style={{ backgroundColor: '#6EB2FB' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="3"
+            style={{ backgroundColor: '#00D3CA' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="4"
+            style={{ backgroundColor: '#FE5C07' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="5"
+            style={{ backgroundColor: '#F8E71C' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="6"
+            style={{ backgroundColor: '#7ED321' }}
+          />
+          <Radio.Button
+            className="oct-radio-button"
+            value="7"
+            style={{ backgroundColor: '#9013FE' }}
+          />
         </Radio.Group>
       </div>
       <div className="oct-attributes">
-        <Title level={4} style={{ color: '#CCC' }}>Size</Title>
+        <Title level={4} style={{ color: '#CCC' }}>
+          Size
+        </Title>
         <Radio.Group defaultValue="a" buttonStyle="outline">
-          <Radio.Button value="1" >XL</Radio.Button>
-          <Radio.Button value="2" >XL</Radio.Button>
-          <Radio.Button value="3" >XL</Radio.Button>
-          <Radio.Button value="4" >XL</Radio.Button>
-          <Radio.Button value="5" >XL</Radio.Button>
-          <Radio.Button value="6" >XL</Radio.Button>
-          <Radio.Button value="7" >XL</Radio.Button>
+          <Radio.Button value="1">XL</Radio.Button>
+          <Radio.Button value="2">XL</Radio.Button>
+          <Radio.Button value="3">XL</Radio.Button>
+          <Radio.Button value="4">XL</Radio.Button>
+          <Radio.Button value="5">XL</Radio.Button>
+          <Radio.Button value="6">XL</Radio.Button>
+          <Radio.Button value="7">XL</Radio.Button>
         </Radio.Group>
       </div>
-      {product && <div className="oct-attributes oct-quantity">
-        <Title level={4} style={{ color: '#CCC' }}>Quantity</Title>
-        <InputNumber min={1} max={12} defaultValue={2} onChange={onChange} />
-      </div>}
-      {product && 
+      {product && (
         <>
-          <Button type="danger" shape="round" icon="shopping-cart" size="large">Add to cart</Button>
-          <Button type="link" icon="heart">Save for Later</Button>
+          <div className="oct-attributes oct-quantity">
+            <Title level={4} style={{ color: '#CCC' }}>
+              Quantity
+            </Title>
+            <InputNumber min={1} max={12} defaultValue={2} onChange={onChange} />
+          </div>
+          <Button type="danger" shape="round" icon="shopping-cart" size="large">
+            Add to cart
+          </Button>
+          <Button type="link" icon="heart">
+            Save for Later
+          </Button>
         </>
-      }
+      )}
     </>
   );
 };
